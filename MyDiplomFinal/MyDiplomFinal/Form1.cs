14682a3cd54dc304934d3ebf6ffde2743666bf30
Alexander Rimasheuski <Squire_86@mail.ru>
2017-02-19 21:05:31 +0300

    Add new UserControlLibrary and first userControl1

--- MyDiplomFinal/MyDiplomFinal/Form1.cs
+++ MyDiplomFinal/MyDiplomFinal/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserControlLibrary;
+
 
 namespace MyDiplomFinal
 {
     public partial class Form1 : Form
     {
+        public MyFormUserControl1 userControl1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void клиентыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            userControl1 = new MyFormUserControl1();
+            this.Controls.Add(userControl1);
+            userControl1.Top = 25;
+            userControl1.Left = 0;
         }
     }
 }
---
+++ MyDiplomFinal/UserControlLibrary/MyFormUserControl1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlLibrary
+{
+    public partial class MyFormUserControl1 : UserControl
+    {
+        public MyFormUserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
